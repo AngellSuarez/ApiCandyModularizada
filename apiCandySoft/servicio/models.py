af4565a55ae54c4359478c921f76e0a7110024f6
angelsuaz <miguel.angel.cardona.suarez1@gmail.com>
2025-05-13 16:22:04 -0500

    servicios
    arreglado las mayusculas

--- apiCandySoft/servicio/models.py
+++ apiCandySoft/servicio/models.py
@@ -14,11 +14,17 @@ class Servicio(models.Model):
     )
 
     nombre = models.CharField(max_length=40, null=False)
+    
     descripcion = models.TextField()
+    
     precio = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
+    
     estado = models.CharField(max_length=40, null=False, choices=ESTADOS_CHOICES, default="Activo")
+    
     tipo = models.CharField(max_length=40, null=False, choices=TIPO_CHOICES, default="Manicure")  
+    
     url_imagen = models.CharField(max_length=200, null=True, blank=True)
+    
     created_at = models.DateTimeField(default=timezone.now)  
 
     def __str__(self):
--- apiCandySoft/servicio/views.py
+++ apiCandySoft/servicio/views.py
@@ -14,19 +14,20 @@ class ServicioViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         try:
             servicio = self.get_object()
-            if servicio.estado == "activo":
-                servicio.estado = 'inactivo'
+            if servicio.estado == "Activo":
+                servicio.estado = 'Inactivo'
                 servicio.save()
-                return Response({'message':'Servicio desactivado correctamente'}, status=status.HTTP_200_OK)
+                return Response({'message':'Servicio desactivado correctamente'}, status = status.HTTP_200_OK)
             else:
                 servicio.delete()
+                return Response({'message':'servicio eliminado correctamente'},status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({'message':'Servicio eliminado correctamente'}, status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=True, methods=['patch'])
     def cambiar_estado(self, request, pk=None):
         servicio = self.get_object()
-        nuevo_estado = "activo" if servicio.estado == "inactivo" else "inactivo"
+        nuevo_estado = "Activo" if servicio.estado == "Inactivo" else "Inactivo"
         servicio.estado = nuevo_estado
         servicio.save()
         serializer = self.get_serializer(servicio)
